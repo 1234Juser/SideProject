2bd767332a7930de77d2575bcfff87caa3298220
seulgiLee113 <cktncth55@gmall.com>
2025-06-11 15:28:42 +0900

    first commit

---
+++ src/main/java/com/nowpick/backend/NowpickApplication.java
@@ -0,0 +1,13 @@
+package com.nowpick.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowpickApplication {
+    
+    public static void main ( String[] args ) {
+        SpringApplication.run (NowpickApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/nowpick/backend/NowpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowpick.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowpickApplicationTests {
+    
+    @Test
+    void contextLoads ( ) {
+    }
+    
+}
